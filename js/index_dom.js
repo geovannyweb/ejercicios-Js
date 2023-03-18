c5876aa2b736ff90d3dcec2e1abf91ea9eacacdc
Geovanny Camargo <geovannyroman2019@gmail.com>
2023-03-18 01:02:56 -0500

    ejercicios DOM

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,8 +1,15 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
-
-const d = document;
-
+import { alarm, digitalClock } from "./dom/reloj.js";
+import { shortcuts } from "./dom/teclado.js";
+let d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
+    digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
+    alarm("../assets/alarma.mp3.mp3", "#activar-alarma", "#desactivar-alarma")
+    
 });
 
+d.addEventListener("keydown", (e) => {
+    shortcuts(e);
+    
+})
